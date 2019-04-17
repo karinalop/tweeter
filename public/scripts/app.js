8ba881149b0e3caa8833afc49f1888767a2069c4
karinalop <karina.popeye@gmail.com>
2019-04-17 19:06:32 +0000

    Add tweets dynamic tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,89 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+ var createTweetElement = function (tweetData){
+
+ let $tweet = $("<article>").addClass("tweet");
+
+ let $img = $("<img>").addClass("tweet-img").attr("src", tweetData.user.avatars.small);
+ let $h3 = $("<h3>").addClass("tweet-h3").text(tweetData.user.name);
+ let $address = $("<address>").addClass("tweet-address").text(tweetData.user.handle);
+ let $header = $("<header>").addClass("tweet-header");
+ $header.append($img).append($h3).append($address);
+
+ let $p = $("<p>").text(tweetData.content.text);
+ let $footer = $("<footer>").addClass("tweet-footer").text(tweetData.created_at);
+
+ $tweet.append($header).append($p).append($footer);
+
+
+ return $tweet;
+}
+
+
+// Fake data taken from tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+//----------------------------------------------
+
+function renderTweets(tweets){
+  tweets.forEach(element =>{
+    $('#tweets-container').append(createTweetElement(element));
+  });
+}
+
+// Test / driver code (temporary)
+
+
+$(document).ready(function(){
+  renderTweets(data);
+
+});
+
--- server/index.js
+++ server/index.js
@@ -39,3 +39,9 @@ app.post("/tweets", (req, res) => {
   console.log("got here")
 
 });
+
+var createTweetElement = function (object){
+
+ var $tweet = $("<article>").addClass("tweet");
+
+}
